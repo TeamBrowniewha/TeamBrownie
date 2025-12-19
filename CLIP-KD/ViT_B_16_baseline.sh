0d3bbb971adb0f10d7711fb562cfffbb901e6cdd
Rosieyang01 <144993283+Rosieyang01@users.noreply.github.com>
2025-12-19 23:24:11 +0900

    Create ViT_B_16_baseline.sh

---
+++ CLIP-KD/ViT_B_16_baseline.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd /home/brownieewha/CLIP-KD/src
+torchrun --nproc_per_node 1 -m \
+    --master_addr=127.0.0.1 --master_port=29534 \
+    training.main -- \
+    --save-frequency 1 \
+    --zeroshot-frequency 1 \
+    --report-to tensorboard \
+    --train-data="/home/brownieewha/CLIP-KD/data/cc3m/cc3m_train.csv"  \
+    --val-data="/home/brownieewha/CLIP-KD/data/cc3m/cc3m_val.csv"  \
+    --data-root "/home/brownieewha/CLIP-KD/data/cc3m/images/images" \
+    --val-data-root "/home/brownieewha/CLIP-KD/data/cc3m/images/images" \
+    --csv-img-key filepath \
+    --csv-caption-key title \
+    --imagenet-val="/home/brownieewha/CLIP-KD/data/imageNet_val" \
+    --warmup 10000 \
+    --batch-size=128 \
+    --lr=1.25e-3 \
+    --wd=0.1 \
+    --epochs 32 \
+    --workers=8 \
+    --model ViT-B-16 \
+    --logs "/home/brownieewha/CLIP-KD/logs/vit_b_16_cc3m_baseline/" \
+    --tag cc3m-cc12m-baseline
